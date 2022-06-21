efa6ff6593eea5a2142910246af74d1a887675c7
BridgePress <lbammargeral@gmail.com>
2022-06-21 23:16:46 +0300

    Add Registration Add Authorization Add Encryptor

---
+++ Authorization.Users/AuthorizationUser.cs
@@ -0,0 +1,31 @@
+﻿using DbCalculatorСalorie.Date;
+using DbCalculatorСalorie.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Text.Encryptor;
+
+namespace Authorization.Users
+{
+    public class AuthorizationUser
+    {
+        public User PasswordCheckAsync(string login,string password)
+        {
+            using (CalculatorСalorieDbContext db = new CalculatorСalorieDbContext())
+            {
+                User findUser = db.Users.FirstOrDefault(x => x.Login == login);
+                if (findUser != null)
+                {
+                    if (findUser.Password == Encrypt.EncryptStr(password) )
+                    {
+                        return findUser;
+                    }
+                }
+            }
+          return null;
+        }
+    }
+}
---
+++ Registration.Users/RegistrationUser.cs
@@ -0,0 +1,41 @@
+﻿using DbCalculatorСalorie.Date;
+using DbCalculatorСalorie.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Text.Encryptor;
+
+namespace Registration.Users
+{
+    public class RegistrationUser
+    {
+        public bool CreateUser(string name, string login, string password, int gender, int age, decimal weight, decimal height)
+        {
+            using (CalculatorСalorieDbContext db = new CalculatorСalorieDbContext())
+            {
+                List<User> users = db.Users.ToList();
+                var serchUser = db.Users.FirstOrDefault(x => x.Name == name);
+                if (serchUser == null)
+                {
+                    User user = new User();
+                    user.Name = name;
+                    user.Login = login;
+                    user.Password = Encrypt.EncryptStr(password);
+                    user.Gender = gender;
+                    user.Age = age;
+                    user.Weight = weight;
+                    user.Height = height;
+                    db.Users.Add(user);
+                    db.SaveChangesAsync();
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+        }
+    }
+}
