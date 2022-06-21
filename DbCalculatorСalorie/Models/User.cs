4cbfaa34b7be10c40d44f1547457f6c50d618d03
BridgePress <lbammargeral@gmail.com>
2022-06-21 22:38:31 +0300

    Add database

---
+++ DbCalculatorСalorie/Models/User.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DbCalculatorСalorie.Models
+{
+    public class User
+    {
+        public int id { get; set; }
+        public string Name { get; set; }
+        public string Login { get; set; }
+        public string Password { get; set; }
+        public int Gender { get; set; }
+        public int Age { get; set; }
+        public decimal Weight { get; set; }
+        public decimal Height { get; set; }
+    }
+}
