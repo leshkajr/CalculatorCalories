0e9432da5cb5b3cd3023dc9bd1c53484891ba296
BridgePress <lbammargeral@gmail.com>
2022-06-22 09:52:44 +0300

    UserConnectDay

---
+++ DbCalculatorСalorie/Migrations/202206220648536_UserConnectDay.cs
@@ -0,0 +1,22 @@
+﻿namespace DbCalculatorСalorie.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class UserConnectDay : DbMigration
+    {
+        public override void Up()
+        {
+            AddColumn("dbo.DietForTheDays", "UserId", c => c.Int());
+            CreateIndex("dbo.DietForTheDays", "UserId");
+            AddForeignKey("dbo.DietForTheDays", "UserId", "dbo.Users", "id");
+        }
+        
+        public override void Down()
+        {
+            DropForeignKey("dbo.DietForTheDays", "UserId", "dbo.Users");
+            DropIndex("dbo.DietForTheDays", new[] { "UserId" });
+            DropColumn("dbo.DietForTheDays", "UserId");
+        }
+    }
+}
