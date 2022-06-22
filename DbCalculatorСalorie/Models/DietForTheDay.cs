a858ba9e4812ef6d96696334049e9f42c3c8ed61
BridgePress <lbammargeral@gmail.com>
2022-06-22 09:37:08 +0300

    Add class DietForTheDay

---
+++ DbCalculatorСalorie/Models/DietForTheDay.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DbCalculatorСalorie.Models
+{
+    public class DietForTheDay
+    {
+        public int id { get; set; }
+        public DateTime Date { get; set; }
+        public ICollection<Product> Products { get; set; }
+        public DietForTheDay()
+        {
+            Products = new List<Product>();
+        }
+    }
+}
