fb888ce2a04dadcfa152af0a21e2062dafb4235d
BridgePress <lbammargeral@gmail.com>
2022-06-22 19:51:37 +0300

    add weight in product

---
+++ DbCalculatorСalorie/Migrations/202206221650303_addWeightinProduct.cs
@@ -0,0 +1,18 @@
+﻿namespace DbCalculatorСalorie.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class addWeightinProduct : DbMigration
+    {
+        public override void Up()
+        {
+            AddColumn("dbo.Products", "Weight", c => c.Double(nullable: false));
+        }
+        
+        public override void Down()
+        {
+            DropColumn("dbo.Products", "Weight");
+        }
+    }
+}
