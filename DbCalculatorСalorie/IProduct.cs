294a8b627c1b811804f7c9f5bb5d09776c11e821
BridgePress <lbammargeral@gmail.com>
2022-06-23 21:59:32 +0300

    Add propduct Add Category

---
+++ DbCalculatorСalorie/IProduct.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DbCalculatorСalorie
+{
+    public interface IProduct
+    {
+        void AddProduct(string name, int protein, int carbohydrates, int fats, int CategoryId);
+        void EditProduct();
+        void RemoveProduct();
+    }
+}
