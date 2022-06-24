ec2b4099482f138bc7f44c8fe6b699ff7cb307c9
BridgePress <lbammargeral@gmail.com>
2022-06-24 20:20:31 +0300

    Up

--- Interface/RegistrWindow.xaml.cs
+++ Interface/RegistrWindow.xaml.cs
@@ -32,7 +32,7 @@ namespace Interface
             {
                 Gender = 0;
             }
-            if (userRepeatPassword.Password.ToString()== userPassword.Password.ToString())
+            if (userRepeatPassword.Password.ToString() == userPassword.Password.ToString())
             {
                 if (RegistrationUser.CreateUser(userName.Text, userLogin.Text, userPassword.Password.ToString(), Gender,int.Parse(userAge.Text), double.Parse(userWeight.Text), double.Parse(userHaight.Text)))
                 {
